80ceed32be7ea77c265fc3cca5986ac029a71863
Ibtisam Afzal <chaudhryibtisam2003@gmail.com>
2023-12-25 23:07:19 +0500

    Changes

--- _3c3ec51e972ae18cabe2019f6aa77791_OOPStarterCode/UCSDUnfoldingMaps/src/module4/CityMarker.java
+++ _3c3ec51e972ae18cabe2019f6aa77791_OOPStarterCode/UCSDUnfoldingMaps/src/module4/CityMarker.java
@@ -9,7 +9,7 @@ import processing.core.PGraphics;
 /** Implements a visual marker for cities on an earthquake map
  * 
  * @author UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+ * @author Ibtisam Afzal
  *
  */
 public class CityMarker extends SimplePointMarker {
